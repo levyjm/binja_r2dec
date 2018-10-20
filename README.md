493888b323bc1c8309af57c6c3be0a4fd639f680
John Levy <levyjohnm@gmail.com>
2018-10-20 12:00:27 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# binja_r2dec
+
+### What this is
+
+This is a simple plugin that just invokes the radare2 plugin, `r2dec`, which is an awesome (and free!) decompiler.
+
+### Requirements
+
+You must have radare2, r2dec, and r2pipe installed. Details on installing radare2 can be found [here](https://github.com/radare/radare2) and details on installing r2dec [here](https://github.com/wargio/r2dec-js).
+
+Then to install r2pipe, run:
+
+```
+$ pip install r2pipe --user
+```
+
+Lastly, once radare2 is installed, you must make sure `radare2` is in Binary Ninja's `PATH`. I've commented out two lines in the `__init__.py` file that you can edit to fix this issue.
---
+++ __init__.py
@@ -0,0 +1,25 @@
+import os
+import json
+from binaryninja import *
+import r2pipe
+
+def run_plugin(bv, addr):
+    func_start = addr.start
+    
+    #r2_path = '/home/ur_name_lols/bin/'
+    #os.environ["PATH"] += os.pathsep + r2_path
+
+    try:
+        r2 = r2pipe.open(bv.file.original_filename)
+        r2.cmd('aaa')
+        r2.cmd('s ' + str(func_start))
+        decompiled_func = r2.cmd('pdd')
+        bv.show_html_report('Decompiled ' + addr.symbol.name, decompiled_func)
+        r2.quit()
+    except Exception as err:
+        if str(err) == "ERROR: Cannot find radare2 in PATH":
+            print("Please add the location of your radare2 binary to your PATH variable")
+
+PluginCommand.register_for_function(
+  "Decompile function",
+  "Decompile function using r2dec", run_plugin)
