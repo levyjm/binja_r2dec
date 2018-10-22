96f258904b9fb4b8692afc294ce628e6733fd441
John Levy <levyjohnm@gmail.com>
2018-10-22 16:10:35 -0400

    adding use of pygments to syntax highlight the code

--- __init__.py
+++ __init__.py
@@ -1,11 +1,23 @@
 import os
-from binaryninja import *
 import r2pipe
+from binaryninja import show_html_report, PluginCommand
+from pygments import highlight
+from pygments.lexers import CLexer
+from pygments.formatters import HtmlFormatter
+from pygments.styles.native import NativeStyle
+
+def show_output(bv, func_name, decompiled):
+    lexer = CLexer()
+    style = NativeStyle()
+    style.background_color = '#272811'
+    formatter = HtmlFormatter(full=True, style='native', noclasses=True)
+    colored_code = highlight(decompiled, lexer, formatter)
+    bv.show_html_report('Decompiled ' + func_name, colored_code)
 
 def run_plugin(bv, addr):
     func_start = addr.start
     
-    #r2_path = '/home/ur_name_lols/bin/'
+    #r2_path = '/home/username/bin/'
     #os.environ["PATH"] += os.pathsep + r2_path
 
     try:
@@ -13,7 +25,10 @@ def run_plugin(bv, addr):
         r2.cmd('aaa')
         r2.cmd('s ' + str(func_start))
         decompiled_func = r2.cmd('pdd')
-        bv.show_html_report('Decompiled ' + addr.symbol.name, decompiled_func)
+        if decompiled_func.startswith('Error: no data available'):
+            r2.cmd('af @ ' + str(func_start))
+            decompiled_func = r2.cmd('pdd')
+        show_output(bv, addr.symbol.name, decompiled_func)
         r2.quit()
     except Exception as err:
         if str(err) == "ERROR: Cannot find radare2 in PATH":
@@ -24,3 +39,4 @@ def run_plugin(bv, addr):
 PluginCommand.register_for_function(
   "Decompile function",
   "Decompile function using r2dec", run_plugin)
+
